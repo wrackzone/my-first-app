f7e69dcc7addbe956d4c5f0ee3833f967b85d930
Barry Mullan <mullan@gmail.com>
2013-07-03 15:03:39 -0400

    my changes

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+    
+    items : [
+        {
+            xtype: 'rallybutton',
+            text: 'Click me',
+            handler: function() {
+                Ext.Msg.alert('Button', 'You clicked me');
+            }
+        }
+    ],
 
     launch: function() {
         //Write app code here
